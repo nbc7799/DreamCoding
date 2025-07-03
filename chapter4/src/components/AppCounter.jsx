8cc3ca976f508211a21aea526236b278619a2076
Jun <nbc7799@naver.com>
2025-07-03 17:50:35 +0900

    counter컴포넌트 하나로 수정 click 함수 구현

--- chapter4/src/components/AppCounter.jsx
+++ chapter4/src/components/AppCounter.jsx
@@ -1,14 +1,8 @@
 import { useState } from "react";
-import Counter from "./Counter1";
 import Counter1 from "./Counter1";
-import Counter2 from "./counter2";
 
 export default function AppCounter() {
   const [totalCount, setTotalCount] = useState(0);
-
-  const handleClick = () => {
-    setTotalCount((prev) => prev + 1);
-  };
 
   return (
     <div>
@@ -16,16 +10,8 @@ export default function AppCounter() {
         Total Count: {totalCount} {totalCount > 10 ? "🔥" : "🧊"}
       </div>
       <div>
-        <Counter1
-          totalCount={totalCount}
-          setTotalCount={setTotalCount}
-          handleClick={handleClick}
-        />
-        <Counter2
-          totalCount={totalCount}
-          setTotalCount={setTotalCount}
-          handleClick={handleClick}
-        />
+        <Counter1 totalCount={totalCount} setTotalCount={setTotalCount} />
+        <Counter1 totalCount={totalCount} setTotalCount={setTotalCount} />
       </div>
     </div>
   );
--- chapter4/src/components/Counter1.jsx
+++ chapter4/src/components/Counter1.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
-export default function Counter1({ handleClick, setTotalCount, totalCount }) {
+export default function Counter1({ setTotalCount }) {
   const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setTotalCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+  };
 
   return (
     <div className="w-[250px] h-[250px] border-red-100  text-center flex flex-col gap-10 items-center justify-center">
       <span className="text-9xl font-bold">{count}</span>
       <button
         className="text-3xl font-bold border-blue-950"
-        onClick={() => {
-          setTotalCount(totalCount + 1);
-          setCount(count + 1);
-        }}
+        onClick={handleClick}
       >
         Add +
       </button>
